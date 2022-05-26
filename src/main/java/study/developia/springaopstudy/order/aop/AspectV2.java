77b5b7484849f56a21d5d726ce61b9f4d71e4674
developiaa <developiaa@gmail.com>
2022-05-26 23:38:41 +0900

    feat : Separate pointcut

--- src/main/java/study/developia/springaopstudy/order/aop/AspectV2.java
+++ src/main/java/study/developia/springaopstudy/order/aop/AspectV2.java
@@ -4,13 +4,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Slf4j
 @Aspect
-public class AspectV1 {
+public class AspectV2 {
 
     // 패키지와 그 하위 패키지
-    @Around("execution(* study.developia.springaopstudy.order..*(..))")
+    // 포인트컷을 분리하여 여러 어드바이스에서 사용할 수 있도록 함
+    @Pointcut("execution(* study.developia.springaopstudy.order..*(..))")
+    private void allOrder() {   // pointcut signature
+
+    }
+
+    @Around("allOrder()")
     public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
         log.info("[log] {}", joinPoint.getSignature()); // join point 시그니처
         return joinPoint.proceed();
