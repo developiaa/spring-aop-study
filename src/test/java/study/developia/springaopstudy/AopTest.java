cf0948433e718674512be8c85fa1caf48276b0fc
developiaa <developiaa@gmail.com>
2022-05-27 22:30:40 +0900

    feat : Add aop other annotation

--- src/test/java/study/developia/springaopstudy/AopTest.java
+++ src/test/java/study/developia/springaopstudy/AopTest.java
@@ -17,7 +17,8 @@ import study.developia.springaopstudy.order.aop.*;
 //@Import(AspectV2.class)
 //@Import(AspectV3.class)
 //@Import(AspectV4Pointcut.class)
-@Import({AspectV5Order.LogAspect.class, AspectV5Order.TransactionAspect.class})
+//@Import({AspectV5Order.LogAspect.class, AspectV5Order.TransactionAspect.class})
+@Import(AspectV6Advice.class)
 public class AopTest {
 
     @Autowired
@@ -43,3 +44,5 @@ public class AopTest {
                 .isInstanceOf(IllegalStateException.class);
     }
 }
+
+
