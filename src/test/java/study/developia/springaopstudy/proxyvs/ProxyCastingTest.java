f3005bc57931048e3a0c7ad8b31c25f3d58bb2d8
developiaa <developiaa@gmail.com>
2022-06-16 23:55:30 +0900

    feat : Add proxy type casting

---
+++ src/test/java/study/developia/springaopstudy/proxyvs/ProxyCastingTest.java
@@ -0,0 +1,40 @@
+package study.developia.springaopstudy.proxyvs;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactory;
+import study.developia.springaopstudy.member.MemberService;
+import study.developia.springaopstudy.member.MemberServiceImpl;
+
+@Slf4j
+public class ProxyCastingTest {
+
+    @Test
+    void jdkProxy() {
+        MemberServiceImpl target = new MemberServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(false); // JDK 동적 프록시
+
+        //프록시를 인터페이스로 캐스팅 성공
+        MemberService memberServiceProxy = (MemberService) proxyFactory.getProxy();
+
+        //JDK 동적 프록시를 구현 클래스로 캐스팅하면 실패, ClasCastException 예외 발생
+        Assertions.assertThrows(ClassCastException.class, () -> {
+            MemberServiceImpl castingMemberService = (MemberServiceImpl) memberServiceProxy;
+        });
+    }
+
+    @Test
+    void cglibProxy() {
+        MemberServiceImpl target = new MemberServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(true); // CGLIB 프록시
+
+        //프록시를 인터페이스로 캐스팅 성공
+        MemberService memberServiceProxy = (MemberService) proxyFactory.getProxy();
+
+        //CGLIB 프록시를 구현 클래스로 캐스팅하면 성공, ClasCastException 예외 발생
+        MemberServiceImpl castingMemberService = (MemberServiceImpl) memberServiceProxy;
+    }
+}
